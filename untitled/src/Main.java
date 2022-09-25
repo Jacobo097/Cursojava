178dbc4e6cf8f269c8573c559618895e7ba8988a
Fernando Daniel Jacobo Vite <daniel-JV@outlook.com>
2022-09-24 20:34:43 -0500

    Nuevo proyecto Java

---
+++ untitled/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //Instancia o objeto
+        Numeros num = new Numeros();
+
+        System.out.println(num.numeros(34));
+
+        System.out.println(num.perro("letras"));
+
+    }
+}
---
+++ untitled/src/Numeros.java
@@ -0,0 +1,17 @@
+
+//clase
+public class Numeros {
+
+    // metodo 2 que regresa enteros
+    public int numeros(int num){
+        int num1= num;
+        return num1;
+    }
+
+    //metodo uno que regresa palabras
+    public String perro(String palabra) { //iniccio
+        return  palabra;
+    } //fin
+
+
+}
